f5e52d8f2bcd164f5e0770263cfff3a9f205e4dd
vikas srivastava <vikas.abes@gmail.com>
2017-06-19 18:33:59 -0400

    updated modules

--- src/App.js
+++ src/App.js
@@ -1,3 +1,27 @@
-/**
- * Created by viksidada on 6/19/2017.
- */
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className = "App">
+                <div className = "app-title">Reminder Pro</div>
+                <div className = "form-inline">
+                    <div className = "form-group">
+                        <input
+                            className = "form-control"
+                            placeholder="I need to..."
+                            />
+                    </div>
+                    <button
+                        type = "button"
+                        className = "btn btn-success"
+                        >Add Reminder
+                    </button>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default App;
